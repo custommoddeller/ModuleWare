b37005b91c6d5fc6e0ea4612bc6f627297151ce4
custommoddeller <163665395+custommoddeller@users.noreply.github.com>
2024-03-30 23:18:24 +0000

    Update 6872274481.lua

--- 6872274481.lua
+++ 6872274481.lua
@@ -9887,22 +9887,50 @@ end)
 	end)
 
 	
-runFunction(function() -- credits to _dremi on discord for finding the method (godpaster and the other skid skidded it from him)
-	local SetEmote = {}
-	local SetEmoteList = {Value = ''}
-	local oldemote
-	local emo2 = {}
-	SetEmote = GuiLibrary.ObjectsThatCanBeSaved.UtilityWindow.Api.CreateOptionsButton({
-		Name = 'SetEmote',
-		Function = function(calling)
-			if calling then
-				oldemote = lplr:GetAttribute('EmoteTypeSlot1')
-				lplr:SetAttribute('EmoteTypeSlot1', emo2[SetEmoteList.Value])
-			else
-				if oldemote then 
-					lplr:GetAttribute('EmoteTypeSlot1', oldemote)
-					oldemote = nil 
+	runFunction(function() -- credits to _dremi on discord for finding the method (godpaster and the other skid skidded it from him)
+		local SetEmote = {}
+		local SetEmoteList = {Value = ''}
+		local oldemote
+		local emo2 = {}
+		SetEmote = GuiLibrary.ObjectsThatCanBeSaved.UtilityWindow.Api.CreateOptionsButton({
+			Name = 'SetEmote',
+			Function = function(calling)
+				if calling then
+					oldemote = lplr:GetAttribute('EmoteTypeSlot1')
+					lplr:SetAttribute('EmoteTypeSlot1', emo2[SetEmoteList.Value])
+				else
+					if oldemote then 
+						lplr:GetAttribute('EmoteTypeSlot1', oldemote)
+						oldemote = nil 
+					end
 				end
+			end
+		})
+		local emo = {}
+		for i,v in pairs(bedwars.EmoteMeta) do 
+			table.insert(emo, v.name)
+			emo2[v.name] = i
+		end
+		table.sort(emo, function(a, b) return a:lower() < b:lower() end)
+		SetEmoteList = SetEmote.CreateDropdown({
+			Name = 'Emote',
+			List = emo,
+			Function = function(emote)
+				if SetEmote.Enabled then 
+					lplr:SetAttribute('EmoteTypeSlot1', emo2[emote])
+				end
+			end
+		})
+	end)
+
+runFunction(function() -- credits to _dremi on discord for finding the method (godpaster and the other skid skidded it from him)
+	local Clipper = {Enabled = false}
+	Clipper = GuiLibrary.ObjectsThatCanBeSaved.BlatantWindow.Api.CreateOptionsButton({
+		Name = "Clipper",
+		Function = function(callback)
+			if callback then
+				entityLibrary.character.HumanoidRootPart.CFrame = CFrame.new(entityLibrary.character.HumanoidRootPart.CFrame.X, entityLibrary.character.HumanoidRootPart.CFrame.Y - 12, entityLibrary.character.HumanoidRootPart.CFrame.Z)
+				Clipper.ToggleButton(false)
 			end
 		end
 	})
